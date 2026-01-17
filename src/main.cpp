adfbe698bc2307bb454e2f245a4b8aebeb3f7e3e
ADARSH JAISWAL <adarshjaiswalxadyo@gmail.com>
2026-01-17 22:30:58 +0530

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,25 @@
+#define SDL_MAIN_HANDLED
+#include <SDL.h>
+
 #include "Emulator.h"
 #include <iostream>
 #include <exception>
 
 int main(int argc, char* argv[]) {
+    (void)argc;
+    (void)argv;
+
     try {
         Emulator emulator;
-        
+
         if (!emulator.Initialize()) {
             std::cerr << "Failed to initialize emulator!" << std::endl;
             return 1;
         }
-        
+
         emulator.Run();
         emulator.Shutdown();
-        
+
         return 0;
     }
     catch (const std::exception& e) {
@@ -21,3 +27,4 @@ int main(int argc, char* argv[]) {
         return 1;
     }
 }
+
